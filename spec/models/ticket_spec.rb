f3050e7e5f8b19e9b204d97e837d8a2cc8e6e869
yohei <garage.t.msy@gmail.com>
2014-11-19 15:14:06 +0900

    insertのテスト実装

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -33,4 +33,21 @@ RSpec.describe Ticket, :type => :model do
       it { expect(@ticket).not_to be_valid }
     end
   end
+
+  describe 'title:hoge,body:fugaのチケットが１件登録されている場合' do
+    before do
+      ticket = Ticket.new({ title: 'hoge', body: 'fuga' })
+      ticket.save
+      @inserted_ticket = Ticket.find(ticket.id)
+    end
+    it 'チケット総数が1件であること' do
+      expect(Ticket.count).to eq 1
+    end
+    it '登録したチケットのtitleが正しいこと' do
+      expect(@inserted_ticket.title).to eq 'hoge'
+    end
+    it '登録したチケットのbodyが正しいこと' do
+      expect(@inserted_ticket.body).to eq 'fuga'
+    end
+  end
 end
