2b85a85c6535471cdc74b86cf4534b08c40c4e4c
yohei <garage.t.msy@gmail.com>
2014-12-09 21:04:22 +0900

    チケット編集画面のテスト追加

--- spec/features/tickets_spec.rb
+++ spec/features/tickets_spec.rb
@@ -7,7 +7,7 @@ feature "Tickets", :type => :feature do
       expect(page.current_url).to eq tickets_url
     end
     it 'データを登録して一覧画面にアクセス' do
-      FactoryGirl.create(:ticket, {title: 'taitoru', body: 'bodeiiii'})
+     FactoryGirl.create(:ticket, {title: 'taitoru', body: 'bodeiiii'})
       visit tickets_path
       expect(page).to have_content('taitoru')
       expect(page).to have_content('bodeiiii')
@@ -16,6 +16,10 @@ feature "Tickets", :type => :feature do
 
   describe 'チケット新規登録画面' do
     before do
+      @users = []
+      (1..10).each do
+          @users << FactoryGirl.create(:user)
+      end
       visit tickets_path
       click_link_or_button 'New Ticket'
     end
@@ -33,6 +37,49 @@ feature "Tickets", :type => :feature do
       expect(page).to have_css('p#notice')
       expect(page).to have_content('ha-gendattu')
       expect(page).to have_content('merosangoti')
+    end
+    it '新規登録時に担当者をアサインできること' do
+      # puts page.body
+      fill_in 'ticket_title', with: 'ha-gendattu'
+      fill_in 'ticket_body', with: 'merosangoti'
+      select @users[3].nickname, from: 'ticket_assignee_id'
+      click_link_or_button 'Create Ticket'
+      expect(page).to have_css('p#notice')
+      expect(page).to have_content('ha-gendattu')
+      expect(page).to have_content('merosangoti')
+      expect(page).to have_content(@users[3].nickname)
+    end
+  end
+
+  # 宿題
+  describe '編集画面について' do
+    before do
+      @ticket = FactoryGirl.create(:ticket)
+      @user = FactoryGirl.create(:user)
+      visit tickets_path
+      click_link_or_button 'Edit'
+    end
+    it '編集画面にアクセスできる' do
+      expect(page.current_path).to eq '/tickets/%d/edit' % @ticket.id
+    end
+    it '編集画面からデータを変更できる' do
+      fill_in 'ticket_title', with: 'メロメロメロ'
+      fill_in 'ticket_body', with: '全力マン'
+      select @user.nickname, from: 'ticket_assignee_id'
+      check 'ticket_finished'
+      select '2015', from: 'ticket_expires_at_1i'
+      select 'April', from: 'ticket_expires_at_2i'
+      select '25', from: 'ticket_expires_at_3i'
+      select '10', from: 'ticket_expires_at_4i'
+      select '30', from: 'ticket_expires_at_5i'
+      click_link_or_button 'Update Ticket'
+
+      expect(page).to have_css('p#notice')
+      expect(page).to have_content('メロメロメロ')
+      expect(page).to have_content('全力マン')
+      expect(page).to have_content(@user.nickname)
+      expect(page).to have_content('Finished: true')
+      expect(page).to have_content('2015-04-25 10:30:00')
     end
   end
 end
